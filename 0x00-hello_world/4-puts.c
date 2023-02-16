235c6e6a5275d500dd6a1170ee7970284b4fcfe8
dekolayakus <adekolaayinde255@gmail.com>
2023-02-16 04:26:06 -0800

    all task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the text "Programming is like a multilingual puzzle
+ * Return: 0 if executed properly, if not return nonzero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
